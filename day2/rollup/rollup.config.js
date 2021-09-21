3875f3ebd5d6925041b85be9df23f3b325a23bd7
ZZY <1157654912@qq.com>
2021-09-21 15:35:12 +0800

    'fix:rollup文件bug修复'

--- day2/rollup/rollup.config.js
+++ day2/rollup/rollup.config.js
@@ -1,5 +1,7 @@
 import babel from 'rollup-plugin-babel';
-import serve from 'rollup-plugin-serve';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import { terser } from '@rollup-plugin-terser';
 
 export default {
     input: './src/index.js', 
